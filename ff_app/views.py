b9fc91cf725d073e0f4f15638aed3a3f3eb15461
jessysu <jessysu@gmail.com>
2017-10-23 00:31:12 -0700

    init UI

--- ff_app/views.py
+++ ff_app/views.py
@@ -1,2 +1,13 @@
 import MySQLdb
 
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    ds = request.POST.get('ds', '')
+    de = request.POST.get('de', '')
+    print("ds = " + ds)
+    print("de = " + de)
+    rs = {}
+    return render(request, 'home.html', rs)
+    
